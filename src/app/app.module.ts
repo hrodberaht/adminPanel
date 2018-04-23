f4da865a23efe9f02a31daa1a49854c5449f1c31
Robert <seedraev@gmail.com>
2018-04-23 18:16:28 +0200

    repair merge problems

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,25 +4,19 @@ import { FormsModule } from '@angular/forms';
 
 
 import { AppComponent } from './app.component';
-<<<<<<< HEAD
 import { AddProductComponent } from './add-product/add-product.component';
 import { ProductListComponent } from './product-list/product-list.component';
 import { HeaderComponent } from './header/header.component';
-=======
 import { InvoiceComponent } from './invoice/invoice.component';
->>>>>>> 67c0e5fd54a6975ba3299448598c2f6a1addff32
 
 
 @NgModule({
   declarations: [
     AppComponent,
-<<<<<<< HEAD
     AddProductComponent,
     ProductListComponent,
-    HeaderComponent
-=======
+    HeaderComponent,
     InvoiceComponent
->>>>>>> 67c0e5fd54a6975ba3299448598c2f6a1addff32
   ],
   imports: [
     BrowserModule,
