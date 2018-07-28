078de2817d5363eae7f2c12a6afff9fd5f95644c
Robert <seedraev@gmail.com>
2018-07-28 19:02:54 +0200

    repair invoices

--- src/app/invoice/invoice.component.ts
+++ src/app/invoice/invoice.component.ts
@@ -1,28 +1,29 @@
-import { Component, OnInit } from '@angular/core';
-import { Product } from '../models/product.model';
+import { Component, OnInit } from "@angular/core";
+import { Product } from "../models/product.model";
 
 @Component({
-  selector: 'app-invoice',
-  templateUrl: './invoice.component.html',
-  styleUrls: ['./invoice.component.css']
+  selector: "app-invoice",
+  templateUrl: "./invoice.component.html",
+  styleUrls: ["./invoice.component.css"]
 })
 export class InvoiceComponent implements OnInit {
+  constructor() {}
 
-  constructor() { }
-
-  ngOnInit() {
-  }
+  ngOnInit() {}
 
   products: Array<string> = [""];
 
+  onEnter(product, index) {
+    this.products[index] = product;
+  }
+
   onAdd() {
-    this.products.push("")
+    this.products.push("");
     console.log(this.products);
   }
 
   onRemove(index) {
-    this.products.splice(index ,1);
+    this.products.splice(index, 1);
     console.log(index);
   }
-
 }
--- src/app/services/products-list.service.ts
+++ src/app/services/products-list.service.ts
@@ -6,22 +6,28 @@ import { Product } from "../models/product.model";
 @Injectable()
 export class ProductsListService {
   productsChanged = new EventEmitter<Product[]>();
-  private products: Product[] = [new Product("jasiek","22.30","https://www.smyk.com/matex-poszewka-na-poduszke-typu-jasiek-frotte-zolta-40x40-cm.html"),
-                           new Product("jasiek","22.30","https://www.smyk.com/matex-poszewka-na-poduszke-typu-jasiek-frotte-zolta-40x40-cm.html")
+  private products: Product[] = [
+    new Product(
+      "jasiek",
+      "22.30",
+      "https://www.smyk.com/matex-poszewka-na-poduszke-typu-jasiek-frotte-zolta-40x40-cm.html"
+    ),
+    new Product(
+      "jasiek",
+      "22.30",
+      "https://www.smyk.com/matex-poszewka-na-poduszke-typu-jasiek-frotte-zolta-40x40-cm.html"
+    )
   ];
 
-  constructor(private router: Router){
-
-  }
+  constructor(private router: Router) {}
 
   getProducts() {
-      return this.products.slice();//add method slice to work with copy of products
+    return this.products.slice(); //add method slice to work with copy of products
   }
 
   addProduct(product: Product) {
     this.products.push(product);
-    this.productsChanged.emit(this.products.slice());// return copy of products
-    this.router.navigate(['/productList'])
+    this.productsChanged.emit(this.products.slice()); // return copy of products
+    this.router.navigate(["/productList"]);
   }
-    
 }
